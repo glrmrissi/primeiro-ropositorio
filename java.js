a3f2fbb2f08e27205d91b2a0766adcb6c4672783
glrm <102769917+glrmrissi@users.noreply.github.com>
2022-09-23 16:11:55 -0300

    Update java.js

--- java.js
+++ java.js
@@ -2,4 +2,4 @@
                     window.alert ('vai se fuder')
                     window.confirm ('obg')
                     window.prompt ('bom dia')
-           </script> ;
+           </script> 
