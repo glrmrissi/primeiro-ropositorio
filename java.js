06eeca3219c03e35952f8ac3dcacea77ad83c7d0
glrm <102769917+glrmrissi@users.noreply.github.com>
2022-09-23 16:09:23 -0300

    Create java.js

---
+++ java.js
@@ -0,0 +1,5 @@
+            <script>
+                    window.alert ('vai se fuder')
+                    window.confirm ('obg')
+                    window.prompt ('bom dia')
+           </script> ;
