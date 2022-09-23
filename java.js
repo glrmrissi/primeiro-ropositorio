b1d2c811c0ce59fbc1bcd4f2f0ab0b5f76f7e4f9
glrm <102769917+glrmrissi@users.noreply.github.com>
2022-09-23 16:14:41 -0300

    Delete java.js

--- java.js
+++
@@ -1,5 +0,0 @@
-            <script>
-                    window.alert ('vai se fuder')
-                    window.confirm ('obg')
-                    window.prompt ('bom dia')
-           </script> 
